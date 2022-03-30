4760ca3e18afd35005e4dcd5d5893adfff9b7ede
nietup <jakub.nietupski@gmail.com>
2022-03-30 16:00:08 +0200

    init student service

---
+++ StudentService/src/test/kotlin/hello/microservice/studentservice/StudentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package hello.microservice.studentservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
