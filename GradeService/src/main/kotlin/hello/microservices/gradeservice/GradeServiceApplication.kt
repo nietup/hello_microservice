2a708984d7be83e805e35ba0c54893c345bb232b
nietup <jakub.nietupski@gmail.com>
2022-03-30 17:03:41 +0200

    setup eureka

--- GradeService/src/main/kotlin/hello/microservices/gradeservice/GradeServiceApplication.kt
+++ GradeService/src/main/kotlin/hello/microservices/gradeservice/GradeServiceApplication.kt
@@ -2,8 +2,10 @@ package hello.microservices.gradeservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class GradeServiceApplication
 
 fun main(args: Array<String>) {
