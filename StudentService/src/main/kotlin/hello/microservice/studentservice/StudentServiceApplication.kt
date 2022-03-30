b7ca1ccdab8486559d2ac9e78814b167e5b861a6
nietup <jakub.nietupski@gmail.com>
2022-03-30 17:04:30 +0200

    setup eureka for student service

--- StudentService/src/main/kotlin/hello/microservice/studentservice/StudentServiceApplication.kt
+++ StudentService/src/main/kotlin/hello/microservice/studentservice/StudentServiceApplication.kt
@@ -2,10 +2,12 @@ package hello.microservice.studentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
+@EnableEurekaClient
 class StudentServiceApplication {
 	@Bean
 	fun restTemplate() : RestTemplate {
