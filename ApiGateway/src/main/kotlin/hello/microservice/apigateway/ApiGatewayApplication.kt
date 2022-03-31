3f8abd3337b85b9f57b088904b11ed8256013287
nietup <jakub.nietupski@gmail.com>
2022-03-31 15:03:37 +0200

    setup api gateway

--- ApiGateway/src/main/kotlin/hello/microservice/apigateway/ApiGatewayApplication.kt
+++ ApiGateway/src/main/kotlin/hello/microservice/apigateway/ApiGatewayApplication.kt
@@ -2,8 +2,10 @@ package hello.microservice.apigateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
