5f976414dff73331f4e10d6c8f62807359c68543
nietup <jakub.nietupski@gmail.com>
2022-04-28 15:10:22 +0200

    ADD config server application

---
+++ CloudConfigServer/src/main/kotlin/hello/microservices/CloudConfigServer/CloudConfigServerApplication.kt
@@ -0,0 +1,15 @@
+package hello.microservices.CloudConfigServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+class CloudConfigServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CloudConfigServerApplication>(*args)
+}
---
+++ CloudConfigServer/src/test/kotlin/hello/microservices/CloudConfigServer/CloudConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package hello.microservices.CloudConfigServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
