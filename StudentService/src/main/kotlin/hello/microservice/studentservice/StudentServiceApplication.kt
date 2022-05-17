d8e4574fac595f5a8a227bdcd9e9a3d4e1edb694
nietup <jakub.nietupski@gmail.com>
2022-05-17 13:24:30 +0200

    add circuit breaker with yml configuration

--- StudentService/src/main/kotlin/hello/microservice/studentservice/StudentServiceApplication.kt
+++ StudentService/src/main/kotlin/hello/microservice/studentservice/StudentServiceApplication.kt
@@ -1,17 +1,11 @@
 package hello.microservice.studentservice
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
-import io.github.resilience4j.timelimiter.TimeLimiterConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder
-import org.springframework.cloud.client.circuitbreaker.Customizer
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
-import java.time.Duration
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,20 +13,6 @@ class StudentServiceApplication {
     @Bean
     @LoadBalanced
     fun restTemplate() = RestTemplate()
-
-
-    @Bean
-    fun defaultCustomizer(): Customizer<Resilience4JCircuitBreakerFactory> =
-        Customizer { factory ->
-            factory.configureDefault { id ->
-                Resilience4JConfigBuilder(id)
-                    .timeLimiterConfig(
-                        TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(6)).build()
-                    )
-                    .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-                    .build()
-            }
-        }
 }
 
 fun main(args: Array<String>) {
