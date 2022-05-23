d849e0297bd89ea6516dd7f4abf7a88795cee349
nietup <jakub.nietupski@gmail.com>
2022-05-23 18:25:23 +0200

    add timer switch to grade service

--- GradeService/src/main/kotlin/hello/microservices/gradeservice/GradeController.kt
+++ GradeService/src/main/kotlin/hello/microservices/gradeservice/GradeController.kt
@@ -4,13 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.math.abs
 
 @RestController
 @RequestMapping("/grades")
 class GradeController {
+    private var sleepTime = 0L
+
     @GetMapping("/{studentId}")
     fun getGrade(@PathVariable studentId: Int): Grade {
-        Thread.sleep(3000)
+        Thread.sleep(sleepTime)
         return Grade(5.0*Math.random(), "Excellent")
+    }
+
+    @GetMapping("/switch")
+    fun switchSlowdown(): String {
+        sleepTime = abs(sleepTime - 3000L)
+
+        return "sleep time updated to $sleepTime"
     }
 }
