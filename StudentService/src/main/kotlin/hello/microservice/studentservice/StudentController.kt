bf094df5db862ec3f81986ba4c452d5a3d0c5248
nietup <jakub.nietupski@gmail.com>
2022-05-30 18:30:44 +0200

    use annotation circuit breaker

--- StudentService/src/main/kotlin/hello/microservice/studentservice/StudentController.kt
+++ StudentService/src/main/kotlin/hello/microservice/studentservice/StudentController.kt
@@ -1,7 +1,7 @@
 package hello.microservice.studentservice
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import mu.KotlinLogging
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,28 +11,19 @@ import org.springframework.web.client.RestTemplate
 @RestController
 @RequestMapping("/students")
 class StudentController(
-    private val restTemplate: RestTemplate,
-    private val circuitBreakerFactory: Resilience4JCircuitBreakerFactory
+    private val restTemplate: RestTemplate
 ) {
 
     companion object {
         private const val URI = "http://GRADE-SERVICE/grades/"
+        private val logger = KotlinLogging.logger {}
     }
 
-    private val logger = KotlinLogging.logger {}
-
     @GetMapping("/{studentId}")
-    fun getStudentWithGrade(@PathVariable studentId: Int): StudentGrade = circuitBreakerFactory
-        .create("student")
-        .run(
-            {
-                logger.info { "querying grade-service for studentId: $studentId" }
-                val grade = restTemplate.getForObject(URI + studentId, Grade::class.java)
-                StudentGrade(Student("Sheran Aśtar", 0), grade!!)
-            },
-            {
-                logger.error { "problem accessing grade-service" }
-                StudentGrade(Student("fallback", 0), Grade(0.0, ""))
-            }
-        )
+    @CircuitBreaker(name = "student")
+    fun getStudentWithGrade(@PathVariable studentId: Int): StudentGrade {
+        logger.info { "querying grade-service for studentId: $studentId" }
+        val grade = restTemplate.getForObject(URI + studentId, Grade::class.java)
+        return StudentGrade(Student("Sheran Aśtar", 0), grade!!)
+    }
 }
